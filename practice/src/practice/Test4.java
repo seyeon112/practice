6c6cd3ef4bd6bbfe283eb21d1e84c430014c0132
seyeon112 <lilylilyghost@gmail.com>
2025-02-23 08:42:07 +0900

    네번째 커밋

---
+++ practice/src/practice/Test4.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Test4 {
+	public static void main(String[] args) {
+//		byte num = 100;
+//		자료형에 의해 오버플로우 발생
+//		byte num = 128;
+//		short num = 100;
+//		자료형에 의해 오버플로우 발생
+//		short num2 = 32768;
+//		short, byte - 자료형이 달라도 연산 가능, 기본자료형은 결국 메모리에 얼마의 값을 적당히 저장할지 결정.
+//		short sNum = 100;
+//		byte bNum = 50;
+//		System.out.println(sNum + bNum);
+//		byte num1 = 20;
+//		short num2 = 30;
+//		int num3 = 40;
+//		long num4 = 50;
+//		
+//		System.out.println(num1);
+//		System.out.println(num2);
+//		System.out.println(num3);
+//		System.out.println(num4);
+		
+	}
+
+}
