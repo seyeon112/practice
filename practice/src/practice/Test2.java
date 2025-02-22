b981b29bde7af7f0fd83f605955e2645d8d929c6
seyeon112 <lilylilyghost@gmail.com>
2025-02-22 12:59:06 +0900

    두번째 커밋

---
+++ practice/src/practice/Test2.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.printf("저는 뉴진스대학교 %d학년에 재학중입니다!", 3);
+//		2가지 이상의 서식문자를 이용헤 데이터 출력 = 묶어서 순서대로 출력
+		System.out.printf("%d는 첫번째, %f은 두번째, %s은 세번쨰", 1, 2.0, "셋");
+//		n칸만큼 확보한 후, 오른쪽 정렬하여 출력합니다.
+		
+	}
+
+}
