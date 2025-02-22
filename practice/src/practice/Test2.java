eec625f1a160089f4474b66c480478f9fe4cc34e
seyeon112 <lilylilyghost@gmail.com>
2025-02-22 13:27:29 +0900

    세번째 커밋

--- practice/src/practice/Test2.java
+++ practice/src/practice/Test2.java
@@ -1,12 +1,36 @@
 package practice;
 
 public class Test2 {
+//		서식문자
 	public static void main(String[] args) {
 		System.out.printf("저는 뉴진스대학교 %d학년에 재학중입니다!", 3);
 //		2가지 이상의 서식문자를 이용헤 데이터 출력 = 묶어서 순서대로 출력
 		System.out.printf("%d는 첫번째, %f은 두번째, %s은 세번쨰", 1, 2.0, "셋");
 //		n칸만큼 확보한 후, 오른쪽 정렬하여 출력합니다.
-		
+		System.out.println();
+		System.out.printf("%5d", 1);
+		System.out.println();
+		System.out.printf("%5d", 12);
+		System.out.println();
+		System.out.printf("%5d", 123);
+		System.out.println();
+		System.out.printf("%5d", 1234);
+		System.out.println();
+		System.out.printf("%5d", 12345);
+		System.out.println();
+//		%-nd
+		System.out.printf("%-5d", 12345);
+		System.out.println();
+		System.out.printf("%-5d", 1234);
+//		소수점 출력하기(float)
+		System.out.printf("%.1f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.2f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.3f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.4f", 1.1234567);
 	}
+	
 
 }
---
+++ practice/src/practice/Test3.java
@@ -0,0 +1,46 @@
+package practice;
+
+public class Test3 {
+////		변수
+//	public static void main(String[] args) {
+////		메서드 함수 
+////		변수 선언
+////		변수는 수가 아닌 데이터를 담은 공간, 자료형은 타입이라고도 불리며 그 공간의 크기를 지정(메모리에)
+//		String name;
+//		int age;
+//		char bloodType;
+//	}
+////		변수 선언 규칙
+//	public static void main(String[] args) {
+//		String Name; 
+////		String 10top;
+//		String top10;
+////		String #Sharp;
+//		String $dallar;
+////		String true;
+//		String True;
+//	}
+////		변수 대입
+//	public static void main(String[] args) {
+//		int studentAge;
+//		studentAge = 13;
+//		
+//		System.out.println(studentAge);
+//	}
+////		변수를 이용해 다른 변수에 복사힉
+//	public static void main(String[] args) {
+//		int myAge = 20;
+//		int yourAge = myAge;
+//		
+//		System.out.println(myAge);
+//		System.out.println(yourAge);
+//	}
+//	변수 이름을 나이와 관련된 이름으로 규칙에 따라 지어보고, 본인의 나이를 대입해 출력해보세요
+	public static void main(String[] args) {
+		int age = 20;
+		int hisAge = age;
+		System.out.println(age);
+		System.out.println(hisAge);
+	}
+
+}
