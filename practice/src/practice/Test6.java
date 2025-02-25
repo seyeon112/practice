b8fdf28b71470511c775086f0121432adb996dfe
seyeon112 <lilylilyghost@gmail.com>
2025-02-25 11:23:29 +0900

    6번째 커밋

--- practice/src/practice/Test6.java
+++ practice/src/practice/Test6.java
@@ -19,13 +19,35 @@ public class Test6 {
 ////		저장 가능한 범위를 넘어섰기에, 음수부터 왼에서 카운트를 다시 시작한다.
 ////		결론적으로는 잘못된 값이 출력되었다.
 //		System.out.println(b2);
-		double d1 = 1.0e100;
-		float f1 = (float)d1;
-		System.out.println(f1);
+//		double d1 = 1.0e100;
+//		float f1 = (float)d1;
+//		System.out.println(f1);
+//		
+//		double d2 = 1.0e-100;
+//		float f2 = (float)d2;
+//		System.out.println(f2);
+//		int i = 10;
+//		byte b = (byte) i;
+//		System.out.println("[int -> byte의 값] :" + i + " b의 값 :" + b);
+//		
+//		int j = 1000;
+//		byte c = (byte) j;
+//		System.out.println("[int -> byte의 값] :" + j + " c의 값 :" + c);
+		double d1 = 1.1234;
+		float f1 = (float) d1;
+		System.out.println("[double => float] d1의 값 : " + d1 + " f1의 값 :" + f1);
 		
-		double d2 = 1.0e-100;
+		double d2 = 1.0e-50;
 		float f2 = (float)d2;
-		System.out.println(f2);
+		System.out.println("[double => float] d2의 값 : " + d2 + " f2의 값 :" + f2);
+		
+		double d3 = 1.0e100;
+		float f3 = (float) d3;
+		System.out.println("[double => float] d3의 값 : " + d3 + " f3의 값 :" + f3);
+		
+		double d4 = 9.123456789;
+		float f4 = (float) d4;
+		System.out.println("[double => float] d4의 값 : " + d3 + " f4의 값 : " + f4);
 	}
 
 }
---
+++ practice/src/practice/Test7.java
@@ -0,0 +1,34 @@
+package practice;
+import java.util.Scanner;
+
+public class Test7 {
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+////		나이를 입력해주세요
+//		System.out.println("나이를 입력해주세요");
+////		스캐너 메서드를 통해 입력받은 숫자값을 age라는 변수에 저장
+//		int age = scanner.nextInt();
+////		내 나이는 몇세입니다를 서식문자 사용하여 출력. 
+//		System.out.printf("내 나이는 %d세입니다", age);
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		String name, address;
+//		int age;
+//		double weight;
+//		
+//		System.out.println("이름, 주소, 나이, 체중을 빈칸으로 구분하여 순서대로 입력하세요.");
+//		name = scanner.next();
+//		address = scanner.next();
+//		age = scanner.nextInt();
+//		weight = scanner.nextDouble();
+//		
+//		System.out.printf("당신의 이름은 %s입니다.%n", name);
+//		System.out.printf("당신의 주소는 %s입니다.%n", address);
+//		System.out.printf("당신의 나이는 %d입니다.%n", age);
+//		System.out.printf("당신의 몸무게는 %.1f입니다.%n", weight);
+//		
+		String fruit = "Apple";
+		
+		System.out.println("I like" + fruit);
+	}
+}
